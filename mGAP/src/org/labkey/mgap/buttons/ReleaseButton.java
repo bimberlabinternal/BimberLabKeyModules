adc5d7586e9b73f25b1c394d3c01d7f3a755c1ff
Ben Bimber <bbimber@gmail.com>
2019-09-25 14:51:13 +0000

    Fix build
    SVN r64546 |2019-09-25 14:51:13 +0000

--- mGAP/src/org/labkey/mgap/buttons/ReleaseButton.java
+++ mGAP/src/org/labkey/mgap/buttons/ReleaseButton.java
@@ -4,10 +4,12 @@ import org.labkey.api.ldk.table.SimpleButtonConfigFactory;
 import org.labkey.api.module.Module;
 import org.labkey.api.view.template.ClientDependency;
 
+import java.util.LinkedHashSet;
+
 public class ReleaseButton extends SimpleButtonConfigFactory
 {
     public ReleaseButton(Module owner)
     {
-        super(owner, "Create mGAP Release", "mGAP.window.ReleaseWindow.buttonHandler();", ClientDependency.fromList("laboratory.context;/SequenceAnalysis/window/OutputHandlerWindow.js;/mGAP/window/ReleaseWindow.js;sequenceanalysis/field/GenomeFileSelectorField.js"));
+        super(owner, "Create mGAP Release", "mGAP.window.ReleaseWindow.buttonHandler();", new LinkedHashSet<>(ClientDependency.fromList("laboratory.context;/SequenceAnalysis/window/OutputHandlerWindow.js;/mGAP/window/ReleaseWindow.js;sequenceanalysis/field/GenomeFileSelectorField.js")));
     }
 }
