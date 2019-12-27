003b968b7be7edf873550b18c5c3a7f5672734ee
bbimber <M0ndayBlu>
2019-12-27 14:38:11 -0800

    Exacloud queue should be single-select

--- primeseq/src/org/labkey/primeseq/pipeline/ExacloudResourceSettings.java
+++ primeseq/src/org/labkey/primeseq/pipeline/ExacloudResourceSettings.java
@@ -23,7 +23,7 @@ public class ExacloudResourceSettings implements JobResourceSettings
                 ToolParameterDescriptor.create("ram", "RAM (GB)", "The RAM requested for this job", "ldk-integerfield", null, null),
                 ToolParameterDescriptor.create("qos", "Queue Options", "Chose the option that best fits your job needs.  The default queue allows up to 36H, long_jobs are up to 10 days, and very_long_jobs are up to 30 days.", "ldk-simplecombo", new JSONObject(){{
                     put("storeValues", "Default;LongJobs;VeryLongJobs");
-                    put("multiSelect", true);
+                    put("multiSelect", false);
                 }}, null),
                 ToolParameterDescriptor.create("highIO", "Add HighIO Flag", "If selected, --qos=highio will be added, which limits the number of concurrent jobs that can run.", "checkbox", null, null),
                 ToolParameterDescriptor.create("time", "Requested Time (days/hours)", "If provided, this is passed to the --time argument.  This cannot be higher than the limit for your requested partition.  Examples are: '2124', '1-0' (1 day, 0 hours), '10-0' (10 days), and '0-72' (72 hours).  If left blank, this will be automatically assigned.", "textfield", null, null),
