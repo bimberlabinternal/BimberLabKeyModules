88f813a4f5d7afb71a33eb6e5fa5fcdd0a2eb5f8
bbimber <bbimber@gmail.com>
2021-03-07 17:37:50 -0800

    Add action to bulk update filepaths

--- primeseq/src/org/labkey/primeseq/PrimeseqController.java
+++ primeseq/src/org/labkey/primeseq/PrimeseqController.java
@@ -28,6 +28,9 @@ import org.labkey.api.action.SpringActionController;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
 import org.labkey.api.data.ContainerType;
+import org.labkey.api.data.DbScope;
+import org.labkey.api.data.SQLFragment;
+import org.labkey.api.data.SqlExecutor;
 import org.labkey.api.module.Module;
 import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.pipeline.PipeRoot;
@@ -242,6 +245,197 @@ public class PrimeseqController extends SpringActionController
         public URLHelper getSuccessURL(Object o)
         {
             return PageFlowUtil.urlProvider(PipelineUrls.class).urlBegin(getContainer());
+        }
+    }
+
+    @RequiresSiteAdmin
+    public static class UpdateFilePathsAction extends ConfirmAction<UpdateFilePathsForm>
+    {
+        @Override
+        public ModelAndView getConfirmView(UpdateFilePathsForm form, BindException errors) throws Exception
+        {
+            StringBuilder html = new StringBuilder();
+            if (form.getReplacementPrefix() == null)
+            {
+                html.append("This action is designed to bulk update filepaths stored in the database, such as when a folder's file root is updated. This circumvents LabKey's normal file update listeners, and should only be performed if you are certain this is what you want. A reason for this is because the default codepath can be slow with extremely large moves.");
+                html.append("<br><br>");
+                html.append("Enter the following:<br>");
+                html.append("<table>");
+                html.append("<tr><td><label for='sourcePrefix'>File Prefix to Replace:</label></td>");
+                html.append("<td><input name='sourcePrefix' type='text' width='600' value='" + HtmlString.of(form.getSourcePrefix()) + "'></td>");
+                html.append("</tr>");
+                html.append("<td><label for='replacementPrefix'>Replacement:</label></td>");
+                html.append("<td><input name='replacementPrefix' type='text' width='600' value='" + HtmlString.of(form.getReplacementPrefix()) + "'></td>");
+                html.append("</tr></table>");
+                html.append("<br>");
+                html.append("When you hit confirm, you will be given an intermediate page summarizing changes before any changes are actually committed. Note: this could potentially make changes site-wide. Continue?");
+
+                return new HtmlView(HtmlString.unsafe(html.toString()));
+            }
+            else
+            {
+                return new HtmlView(HtmlString.unsafe(generateChangeSummary(form)));
+            }
+        }
+
+        private String generateChangeSummary(UpdateFilePathsForm form)
+        {
+            StringBuilder ret = new StringBuilder();
+            ret.append("You entered the following values:");
+            ret.append("<br>");
+            ret.append("<table>");
+            ret.append("<tr><td><label for='sourcePrefix'>File Prefix to Replace:</label></td>");
+            ret.append("<td>" + HtmlString.of(form.getSourcePrefix()) + "</td>");
+            ret.append("</tr>");
+            ret.append("<td><label for='replacementPrefix'>Replacement:</label></td>");
+            ret.append("<td>" + HtmlString.of(form.getReplacementPrefix()) + "</td>");
+            ret.append("</tr></table>");
+            ret.append("<input type='hidden' name='updateDatabase' value='1'");
+
+            ret.append("The following SQL will be executed. Please check carefully before hitting confirm:");
+            ret.append("<br>");
+            ret.append("<pre>");
+            ret.append(getSql(form, true));
+            ret.append("</pre>");
+            ret.append("<br>");
+            ret.append("Note: if the URL of the folder changed you may also want to execute something like the following (manually):<br>");
+            ret.append("<pre>");
+            ret.append(HtmlString.of("UPDATE pipeline.StatusFiles SET DataUrl = replace(DataUrl, '<SOURCE_FOLDER_URL>', '<REPLACEMENT_FOLDER_URL>') "));
+            ret.append(HtmlString.of("WHERE DataUrl like '%<SOURCE_FOLDER_URL>%';"));
+            ret.append("</pre>");
+
+            ret.append("<br>");
+
+            return ret.toString();
+        }
+
+        private String ensureSlashes(String input)
+        {
+            if (input == null)
+            {
+                return input;
+            }
+
+            if (!input.startsWith("/"))
+            {
+                input = "/" + input;
+            }
+
+            if (!input.endsWith("/"))
+            {
+                input = input + "/";
+            }
+
+            return input;
+        }
+
+        private String getSql(UpdateFilePathsForm form, boolean calculateCounts)
+        {
+            // Ensure start/end with slash:
+            String sourcePrefix = ensureSlashes(form.getSourcePrefix());
+            String replacementPrefix = ensureSlashes(form.getReplacementPrefix());
+
+            StringBuilder sql = new StringBuilder();
+
+            if (calculateCounts)
+            {
+                int count = new SqlExecutor(DbScope.getLabKeyScope()).execute(new SQLFragment("SELECT count(*) FROM Exp.Data WHERE DataFileUrl like 'file://" + sourcePrefix + "%'"));
+                sql.append("--Matching rows: " + count + "\n");
+            }
+
+            sql.append("UPDATE Exp.Data SET DataFileUrl = replace(DataFileUrl, 'file://" + sourcePrefix + "', 'file://" + replacementPrefix + "') ");
+            sql.append("WHERE DataFileUrl like 'file://" + sourcePrefix + "%';\n");
+
+            if (calculateCounts)
+            {
+                int count = new SqlExecutor(DbScope.getLabKeyScope()).execute(new SQLFragment("SELECT count(*) FROM pipeline.StatusFiles WHERE FilePath like '" + sourcePrefix + "%'"));
+                sql.append("--Matching rows: " + count + "\n");
+            }
+            sql.append("UPDATE pipeline.StatusFiles SET FilePath = replace(FilePath, '" + sourcePrefix + "', '" + replacementPrefix + "') ");
+            sql.append("WHERE FilePath like '" + sourcePrefix + "%';");
+
+            return sql.toString();
+        }
+        @Override
+        public boolean handlePost(UpdateFilePathsForm form, BindException errors) throws Exception
+        {
+            if (form.isUpdateDatabase())
+            {
+                String sql = getSql(form, false);
+
+                SqlExecutor se = new SqlExecutor(DbScope.getLabKeyScope());
+                se.execute(new SQLFragment(sql));
+            }
+
+            return true;
+        }
+
+        @Override
+        public void validateCommand(UpdateFilePathsForm form, Errors errors)
+        {
+            if (form.isUpdateDatabase() && form.getReplacementPrefix() == null)
+            {
+                errors.reject(ERROR_MSG, "Missing replacementPrefix");
+            }
+
+            if (form.isUpdateDatabase() && form.getSourcePrefix() == null)
+            {
+                errors.reject(ERROR_MSG, "Missing sourcePrefix");
+            }
+        }
+
+        @Override
+        public @NotNull URLHelper getSuccessURL(UpdateFilePathsForm form)
+        {
+            if (!form.isUpdateDatabase())
+            {
+                ActionURL url = new ActionURL(UpdateFilePathsAction.class, getContainer());
+                url.addParameter("sourcePrefix", form.getSourcePrefix());
+                url.addParameter("replacementPrefix", form.getReplacementPrefix());
+
+                return url;
+            }
+            else
+            {
+                return getContainer().getStartURL(getUser());
+            }
+        }
+    }
+
+    public static class UpdateFilePathsForm
+    {
+        private String _sourcePrefix;
+        private String _replacementPrefix;
+        private boolean _updateDatabase = false;
+
+        public String getSourcePrefix()
+        {
+            return _sourcePrefix;
+        }
+
+        public void setSourcePrefix(String sourcePrefix)
+        {
+            _sourcePrefix = sourcePrefix;
+        }
+
+        public String getReplacementPrefix()
+        {
+            return _replacementPrefix;
+        }
+
+        public void setReplacementPrefix(String replacementPrefix)
+        {
+            _replacementPrefix = replacementPrefix;
+        }
+
+        public boolean isUpdateDatabase()
+        {
+            return _updateDatabase;
+        }
+
+        public void setUpdateDatabase(boolean updateDatabase)
+        {
+            _updateDatabase = updateDatabase;
         }
     }
 }
