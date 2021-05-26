2ce4b9ba9af282b0815108fc66c6f6dab1831a12
bbimber <bbimber@gmail.com>
2021-05-26 12:27:37 -0700

    Use MergeVcfsAndGenotypes over GATK CombineVariants

--- variantdb/src/org/labkey/variantdb/run/MergeVcfsAndGenotypesWrapper.java
+++ variantdb/src/org/labkey/variantdb/run/MergeVcfsAndGenotypesWrapper.java
@@ -2,7 +2,7 @@ package org.labkey.variantdb.run;
 
 import org.apache.logging.log4j.Logger;
 import org.labkey.api.pipeline.PipelineJobException;
-import org.labkey.api.sequenceanalysis.run.AbstractGatkWrapper;
+import org.labkey.api.sequenceanalysis.run.AbstractDiscvrSeqWrapper;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -11,26 +11,22 @@ import java.util.List;
 /**
  * Created by bimber on 8/8/2014.
  */
-public class CombineVariantsWrapper extends AbstractGatkWrapper
+public class MergeVcfsAndGenotypesWrapper extends AbstractDiscvrSeqWrapper
 {
-    public CombineVariantsWrapper(Logger log)
+    public MergeVcfsAndGenotypesWrapper(Logger log)
     {
         super(log);
     }
 
-    public void execute(File referenceFasta, List<File> inputVcfs, File outputVcf, List<String> options) throws PipelineJobException
+    public void execute(File referenceFasta, List<File> inputVcfs, File outputVcf) throws PipelineJobException
     {
-        getLogger().info("Running GATK CombineVariants");
+        getLogger().info("Running MergeVcfsAndGenotypes");
 
         ensureDictionary(referenceFasta);
 
         List<String> args = new ArrayList<>();
-        args.add("java");
-        args.add("-Xmx8g");
-        args.add("-jar");
-        args.add(getJAR().getPath());
-        args.add("-T");
-        args.add("CombineVariants");
+        args.addAll(getBaseArgs());
+        args.add("MergeVcfsAndGenotypes");
         args.add("-R");
         args.add(referenceFasta.getPath());
 
@@ -40,13 +36,8 @@ public class CombineVariantsWrapper extends AbstractGatkWrapper
             args.add(f.getPath());
         }
 
-        args.add("-o");
+        args.add("-O");
         args.add(outputVcf.getPath());
-
-        if (options != null)
-        {
-            args.addAll(options);
-        }
 
         execute(args);
         if (!outputVcf.exists())
