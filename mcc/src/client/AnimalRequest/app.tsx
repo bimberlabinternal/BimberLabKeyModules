2efc60bfa8b701ba37435224ae4223cce6d386e1
bbimber <bbimber@gmail.com>
2021-08-12 18:12:52 -0400

    MCC Animal Request Form (#21)
    * Initial implementation of animal request form 
    
    Co-authored-by: hextraza <sebastiancbenjamin@gmail.com>

---
+++ mcc/src/client/AnimalRequest/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './tailwind.css';
+
+import { AnimalRequest } from './animal-request'
+
+// Need to wait for container element to be available in labkey wrapper before render
+window.addEventListener('DOMContentLoaded', (event) => {
+    ReactDOM.render(<AnimalRequest />, document.getElementById('app'));
+});
---
+++ mcc/src/client/AnimalRequest/input.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Input(props) {
+    if(props.display === false) {
+        return (
+            <>
+            </>
+        )
+    } else {
+        return (
+            <input className="tw-appearance-none tw-block tw-w-full tw-text-gray-700 tw-border-gray-300 tw-rounded tw-py-4 tw-px-6 tw-mb-3 tw-leading-tight focus:tw-outline-none" 
+             name={props.id} id={props.id} type="text" placeholder={props.placeholder} required={props.required}/>
+        )
+    }
+}
