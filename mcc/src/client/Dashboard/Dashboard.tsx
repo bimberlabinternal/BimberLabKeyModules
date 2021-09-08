62bef55b9b4b99f1ceee261ab6c952ab8f5dec5f
bbimber <bbimber@gmail.com>
2021-09-08 14:12:04 -0700

    Add u24_status field to dashboard query

--- mcc/src/client/Dashboard/Dashboard.tsx
+++ mcc/src/client/Dashboard/Dashboard.tsx
@@ -27,7 +27,7 @@ export function Dashboard() {
                 containerPath: containerPath,
                 schemaName: 'study',
                 queryName: 'demographics',
-                columns: 'Id,birth,death,gender/meaning,species,colony,calculated_status,Id/age/AgeFriendly,Id/ageClass/label',
+                columns: 'Id,birth,death,gender/meaning,species,colony,calculated_status,u24_status,Id/age/AgeFriendly,Id/ageClass/label',
                 success: function(results) {
                     setLiving(results.rows.filter(row => row.calculated_status === 'Alive'));
                     setu24Assigned(results.rows.filter(row => row.u24_status === true));
