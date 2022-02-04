cc32769bae15cae0108dfef1f688dee1cc695352
bbimber <bbimber@gmail.com>
2022-02-03 20:01:16 -0800

    Test improvements

--- mcc/test/src/org/labkey/test/tests/mcc/MccTest.java
+++ mcc/test/src/org/labkey/test/tests/mcc/MccTest.java
@@ -306,7 +306,7 @@ public class MccTest extends BaseWebDriverTest
     {
         // NOTE: delay setting module properties until the study exists, since dashboard depends on it
         _containerHelper.createProject(getProjectName());
-        _containerHelper.enableModules(Arrays.asList("mcc", "study"));
+        _containerHelper.enableModules(Arrays.asList("MCC", "Study"));
 
         importStudyFromPath();
 
