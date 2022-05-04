236f2baa02ef34e4efab3d7c1bfb59d3c71fb7bc
bbimber <bbimber@gmail.com>
2022-05-04 08:54:58 -0700

    Minor improvements to MCC review form

--- mcc/src/client/RequestReview/components/InternalReviewForm.tsx
+++ mcc/src/client/RequestReview/components/InternalReviewForm.tsx
@@ -1,9 +1,11 @@
 import React, { FormEvent, useState } from 'react';
 import { Filter, Query } from '@labkey/api';
-import { Box, Button, Table, TableBody, TableCell, TableRow, TextField } from '@material-ui/core';
+import { Box, Button, Dialog, DialogTitle, Table, TableBody, TableCell, TableRow, TextField } from '@material-ui/core';
 
 export default function InternalReviewForm(props: {requestId: string, readOnly?: boolean}) {
     const [ recordData, setRecordData ] = useState(null)
+    const [ showReturnToInvestigator, setShowReturnToInvestigator ] = useState(false)
+    const [ showSendToRab, setShowSendToRab ] = useState(false)
 
     Query.selectRows({
         schemaName: "mcc",
@@ -23,6 +25,7 @@ export default function InternalReviewForm(props: {requestId: string, readOnly?:
             setRecordData(resp.rows[0] ? {...resp.rows[0]} : {})
         },
         failure: function(response) {
+            console.error(response)
             alert(response.exception)
         }
     })
@@ -36,12 +39,9 @@ export default function InternalReviewForm(props: {requestId: string, readOnly?:
     }
 
     const handleChange = (e) => {
-        recordData[e.target.name] = e.target.value ? e.target.value.trim() : null
-        setRecordData({...recordData})
+        setRecordData({...recordData, [e.target.value]: e.target.value ? e.target.value.trim() : null})
 
-        console.log(e.target.name)
-        console.log(e.target.value.trim())
-        console.log(recordData)
+        console.log(recordData[e.target.name])
     };
 
     const onFormSubmit = (e: FormEvent) => {
@@ -60,20 +60,29 @@ export default function InternalReviewForm(props: {requestId: string, readOnly?:
             <Table width={500}>
                 <TableBody>
                 <TableRow>
-                    <TableCell><TextField key={"preliminaryScore"} label={"Preliminary Score"} onChange={handleChange} variant={'outlined'} value={recordData.preliminaryScore || ''} disabled={true} fullWidth={true}/></TableCell>
+                    <TableCell><TextField key={"preliminaryScore"} name={"preliminaryScore"} label={"Preliminary Score"} onChange={handleChange} variant={'outlined'} value={recordData.preliminaryScore || ''} disabled={true} fullWidth={true}/></TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell><TextField key={"resourceAvailabilityScore"} label={"Resource Availability Score"} onChange={handleChange} variant={'outlined'} defaultValue={recordData.resourceAvailabilityScore || ''} fullWidth={true} /></TableCell>
+                    <TableCell><TextField key={"resourceAvailabilityScore"} name={"resourceAvailabilityScore"} label={"Resource Availability Score"} onChange={handleChange} variant={'outlined'} defaultValue={recordData.resourceAvailabilityScore || ''} fullWidth={true} /></TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell><TextField key={"proposalScore"} label={"Final Proposal Score"} onChange={handleChange} variant={'outlined'} defaultValue={recordData.proposalScore || ''} fullWidth={true} /></TableCell>
+                    <TableCell><TextField key={"proposalScore"} name={"proposalScore"} label={"Final Proposal Score"} onChange={handleChange} variant={'outlined'} defaultValue={recordData.proposalScore || ''} fullWidth={true} /></TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell><TextField key={"comments"} label={"Comments"} minRows={4} multiline={true} onChange={handleChange} variant={'outlined'} defaultValue={recordData.comments || ''} fullWidth={true} /></TableCell>
+                    <TableCell><TextField key={"comments"} name={"comments"} label={"Comments"} minRows={4} multiline={true} onChange={handleChange} variant={'outlined'} defaultValue={recordData.comments || ''} fullWidth={true} /></TableCell>
                 </TableRow>
                 </TableBody>
             </Table>
             <Button key={"saveReviewBtn"} variant={"contained"} style={{marginRight: 10}} type={'submit'}>Save Review</Button>
+            <Button key={"returnToPiBtn"} variant={"contained"} style={{marginRight: 10}} type={'button'} onClick={(e) => setShowReturnToInvestigator(true)}>Return to Investigator</Button>
+            <Button key={"rabAssignBtn"} variant={"contained"} style={{marginRight: 10}} type={'button'} onClick={(e) => setShowSendToRab(true)}>Assign to RAB Reviewers</Button>
+            <Dialog open={showReturnToInvestigator}>
+                <DialogTitle>Return To Investigator</DialogTitle>
+
+            </Dialog>
+            <Dialog open={showSendToRab}>
+                <DialogTitle>Assign for RAB Review</DialogTitle>
+            </Dialog>
             </form>
         </Box>
         </>
