6fd14fe5a4110c17a31e20ff29b381227a235f98
bbimber <bbimber@gmail.com>
2022-11-22 05:17:42 -0800

    Fix build

--- LabPurchasing/src/org/labkey/labpurchasing/LabPurchasingDataProvider.java
+++ LabPurchasing/src/org/labkey/labpurchasing/LabPurchasingDataProvider.java
@@ -1,6 +1,6 @@
 package org.labkey.labpurchasing;
 
-import org.json.JSONObject;
+import org.json.old.JSONObject;
 import org.labkey.api.data.Container;
 import org.labkey.api.laboratory.AbstractDataProvider;
 import org.labkey.api.laboratory.DetailsUrlWithoutLabelNavItem;
