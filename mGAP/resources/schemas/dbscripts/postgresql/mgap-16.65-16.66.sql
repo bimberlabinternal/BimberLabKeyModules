51aba400329501e5891c8421786eccabf34d7262
bbimber <bbimber@gmail.com>
2023-07-03 12:02:54 -0700

    Allow more aspects of mGAP annotations to be driven using table

---
+++ mGAP/resources/schemas/dbscripts/postgresql/mgap-16.65-16.66.sql
@@ -0,0 +1,5 @@
+ALTER TABLE mGAP.annotations ADD toolName varchar(1000);
+ALTER TABLE mGAP.annotations ADD formatString varchar(1000);
+ALTER TABLE mGAP.annotations ADD allowableValues varchar(4000);
+ALTER TABLE mGAP.annotations ADD hidden boolean;
+ALTER TABLE mGAP.annotations ADD isIndexed boolean;
