78586033f2513ff8e56dc5cedbf477a9413ee9e3
bbimber <bbimber@gmail.com>
2023-09-12 11:33:21 -0700

    Add setting to control whether kinship import is allowed during business hours

--- PMR/test/src/org/labkey/test/tests/pmr/PMRTest.java
+++ PMR/test/src/org/labkey/test/tests/pmr/PMRTest.java
@@ -137,6 +137,7 @@ public class PMRTest extends BaseWebDriverTest implements SqlserverOnlyTest
         beginAt(getProjectName() + "/ehr-ehrAdmin.view");
         waitAndClickAndWait(Locator.tagContainingText("a", "Genetics Calculations"));
         _ext4Helper.checkCheckbox(Ext4Helper.Locators.checkbox(this, "Kinship validation?:"));
+        _ext4Helper.checkCheckbox(Ext4Helper.Locators.checkbox(this, "Allow Import During Business Hours?:"));
         Locator loc = Locator.inputByIdContaining("numberfield");
         waitForElement(loc);
         setFormElement(loc, "23");
