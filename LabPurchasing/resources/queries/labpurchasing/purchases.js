2ca0f97ed3fb97a9a711914f7d62fb0339de165d
bbimber <bbimber@gmail.com>
2024-04-16 08:37:11 -0700

    Improve validation on requestor

--- LabPurchasing/resources/queries/labpurchasing/purchases.js
+++ LabPurchasing/resources/queries/labpurchasing/purchases.js
@@ -4,6 +4,31 @@ var console = require("console");
 var triggerHelper = new org.labkey.labpurchasing.LabPurchasingTriggerHelper(LABKEY.Security.currentUser.id, LABKEY.Security.currentContainer.id);
 
 function beforeInsert(row, errors){
+    beforeUpsert(row, errors)
+}
+
+function beforeUpdate(row, oldRow, errors){
+    beforeUpsert(row, errors)
+}
+
+function beforeUpsert(row, errors){
+    // Validate requestor:
+    if (row.requestor) {
+        if (!isNaN(row.requestor) && !triggerHelper.isValidUserId(row.requestor)) {
+            errors.requestor = 'Unknown userId for requestor: ' + row.requestor;
+        }
+        // Try to resolve strings:
+        else if (isNaN(row.requestor)) {
+            var id = triggerHelper.resolveUserId(String(row.requestor));
+            if (!id) {
+                errors.requestor = 'Unknown userId for requestor: ' + row.requestor;
+            }
+            else {
+                row.requestor = id;
+            }
+        }
+    }
+
     // The purpose of this is to allow the user to provide a string value for
     // vendorId or vendorName, and attempt to resolve this against known vendors:
     if (!row.vendorId || isNaN(row.vendorId)) {
