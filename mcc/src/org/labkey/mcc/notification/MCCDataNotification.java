ec8e5d4079898b3f914c654262576715ac30042c
bbimber <bbimber@gmail.com>
2024-07-25 20:13:38 -0700

    Improve MCC notification

--- mcc/src/org/labkey/mcc/notification/MCCDataNotification.java
+++ mcc/src/org/labkey/mcc/notification/MCCDataNotification.java
@@ -1,8 +1,13 @@
 package org.labkey.mcc.notification;
 
+import org.labkey.api.data.CompareType;
 import org.labkey.api.data.Container;
+import org.labkey.api.data.SimpleFilter;
+import org.labkey.api.data.TableInfo;
+import org.labkey.api.data.TableSelector;
 import org.labkey.api.ehr.notification.AbstractEHRNotification;
 import org.labkey.api.module.Module;
+import org.labkey.api.query.FieldKey;
 import org.labkey.api.security.User;
 
 import java.util.Date;
@@ -54,19 +59,88 @@ public class MCCDataNotification extends AbstractEHRNotification
     @Override
     public String getMessageBodyHTML(Container c, User u)
     {
-        Map<String, String> saved = getSavedValues(c);
-        Map<String, String> toSave = new HashMap<String, String>();
-
         StringBuilder msg = new StringBuilder();
+        Date now = new Date();
 
-        // TODO:
-        // missing IDs
-        // U24 assigned and dead?
-        // available for transfer and dead?
-        // dam/sire wrong sex
+        doParentSexCheck(c, u, msg);
+        doU24AssignedCheck(c, u , msg);
+        doMissingIdCheck(c, u, msg);
 
-        saveValues(c, toSave);
+        //since we dont want to trigger an email if there's no alerts, conditionally append the title
+        if (msg.length() > 0)
+        {
+            msg.insert(0, "This email contains a series of automatic alerts about the MCC.  It was run on: " + getDateFormat(c).format(now) + " at " + AbstractEHRNotification._timeFormat.format(now) + ".<p>");
+        }
 
         return msg.toString();
+    }
+
+    protected void doMissingIdCheck(final Container c, User u, final StringBuilder msg)
+    {
+        TableInfo ti = getStudySchema(c, u).getTable("aggregatedDemographics");
+
+        SimpleFilter filter = new SimpleFilter(FieldKey.fromString("Id"), null, CompareType.ISBLANK);
+        TableSelector ts = new TableSelector(ti, filter, null);
+        long count = ts.getRowCount();
+        if (count > 0)
+        {
+            msg.append("<b>WARNING: There are ").append(count).append(" animals missing MCC IDs\n");
+            msg.append("<p><a href='").append(getExecuteQueryUrl(c, "study", "demographics", null)).append("&").append(filter.toQueryString("query")).append("'>Click here to view them</a><br>\n\n");
+            msg.append("<hr>\n\n");
+        }
+    }
+
+    protected void doU24AssignedCheck(final Container c, User u, final StringBuilder msg)
+    {
+        TableInfo demographics = getStudySchema(c, u).getTable("demographics");
+
+        SimpleFilter filter = new SimpleFilter(FieldKey.fromString("availability"), "available for transfer", CompareType.EQUAL);
+        filter.addCondition(FieldKey.fromString("calculated_status"), "Alive", CompareType.NEQ_OR_NULL);
+
+        TableSelector ts = new TableSelector(demographics, filter, null);
+        long count = ts.getRowCount();
+        if (count > 0)
+        {
+            msg.append("<b>WARNING: There are ").append(count).append(" animals marked 'available for transfer' that are not alive\n");
+            msg.append("<p><a href='").append(getExecuteQueryUrl(c, "study", "demographics", null)).append("&").append(filter.toQueryString("query")).append("'>Click here to view them</a><br>\n\n");
+            msg.append("<hr>\n\n");
+        }
+
+        filter = new SimpleFilter(FieldKey.fromString("u24_status"), true, CompareType.EQUAL);
+        filter.addCondition(FieldKey.fromString("calculated_status"), "Alive", CompareType.NEQ_OR_NULL);
+
+        ts = new TableSelector(demographics, filter, null);
+        count = ts.getRowCount();
+        if (count > 0)
+        {
+            msg.append("<b>WARNING: There are ").append(count).append(" U24-assigned animals that are not alive\n");
+            msg.append("<p><a href='").append(getExecuteQueryUrl(c, "study", "demographics", null)).append("&").append(filter.toQueryString("query")).append("'>Click here to view them</a><br>\n\n");
+            msg.append("<hr>\n\n");
+        }
+    }
+
+    protected void doParentSexCheck(final Container c, User u, final StringBuilder msg)
+    {
+        TableInfo demographics = getStudySchema(c, u).getTable("demographics");
+
+        SimpleFilter filter = new SimpleFilter(FieldKey.fromString("dam/gender/origgender"), "f", CompareType.NEQ_OR_NULL);
+        TableSelector ts = new TableSelector(demographics, filter, null);
+        long count = ts.getRowCount();
+        if (count > 0)
+        {
+            msg.append("<b>WARNING: There are ").append(count).append(" dams with gender not equal to f</b><br>\n");
+            msg.append("<p><a href='").append(getExecuteQueryUrl(c, "study", "demographics", null)).append("&").append(filter.toQueryString("query")).append("'>Click here to view them</a><br>\n\n");
+            msg.append("<hr>\n\n");
+        }
+
+        filter = new SimpleFilter(FieldKey.fromString("sire/gender/origgender"), "m", CompareType.NEQ_OR_NULL);
+        ts = new TableSelector(demographics, filter, null);
+        count = ts.getRowCount();
+        if (count > 0)
+        {
+            msg.append("<b>WARNING: There are ").append(count).append(" sires with gender not equal to f</b><br>\n");
+            msg.append("<p><a href='").append(getExecuteQueryUrl(c, "study", "demographics", null)).append("&").append(filter.toQueryString("query")).append("'>Click here to view them</a><br>\n\n");
+            msg.append("<hr>\n\n");
+        }
     }
 }
