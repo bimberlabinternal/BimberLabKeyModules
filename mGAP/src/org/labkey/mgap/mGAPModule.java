6ea56e7125c033cd447a3cbe761a6df9ad690894
bbimber <bbimber@gmail.com>
2024-11-12 10:24:31 -0800

    Add additional mGAP track columns

--- mGAP/src/org/labkey/mgap/mGAPModule.java
+++ mGAP/src/org/labkey/mgap/mGAPModule.java
@@ -76,7 +76,7 @@ public class mGAPModule extends ExtendedSimpleModule
     @Override
     public Double getSchemaVersion()
     {
-        return 16.74;
+        return 16.75;
     }
 
     @Override
