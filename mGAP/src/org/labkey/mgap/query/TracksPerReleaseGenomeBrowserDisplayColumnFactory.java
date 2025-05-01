180be0c4658254e3a39b1c6ed938a4735575988f
bbimber <bbimber@gmail.com>
2025-05-01 13:08:11 -0700

    Include species in TracksPerReleaseGenomeBrowserDisplayColumnFactory

--- mGAP/src/org/labkey/mgap/query/TracksPerReleaseGenomeBrowserDisplayColumnFactory.java
+++ mGAP/src/org/labkey/mgap/query/TracksPerReleaseGenomeBrowserDisplayColumnFactory.java
@@ -5,10 +5,12 @@ import org.labkey.api.data.ContainerManager;
 import org.labkey.api.data.DisplayColumn;
 import org.labkey.api.data.RenderContext;
 import org.labkey.api.query.DetailsURL;
+import org.labkey.api.query.FieldKey;
 import org.labkey.api.util.PageFlowUtil;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Set;
 
 /**
  * Created by bimber on 5/17/2017.
@@ -26,12 +28,25 @@ public class TracksPerReleaseGenomeBrowserDisplayColumnFactory extends VariantRe
                 String jbrowseId = ctx.get(getBoundKey("releaseId", "jbrowseId"), String.class);
                 String containerId = ctx.get(getBoundKey("releaseId", "container"), String.class);
                 String trackName = ctx.get(getBoundKey("trackName"), String.class);
+                String species = ctx.get(getBoundKey("releaseId", "species"), String.class);
 
                 if (jbrowseId != null && trackName != null)
                 {
-                    DetailsURL url = DetailsURL.fromString("/mgap/genomeBrowser.view?database=" + jbrowseId + "&activeTracks=" + trackName, ContainerManager.getForId(containerId));
+                    DetailsURL url = DetailsURL.fromString("/mgap/genomeBrowser.view?database=" + jbrowseId + "&activeTracks=" + trackName + (species == null ? "" : "&nhpSpecies=" + species), ContainerManager.getForId(containerId));
                     out.write("<a class=\"labkey-text-link\" href=\"" + url.getActionURL().getURIString() + "\");\">View In Genome Browser</a>");
                 }
+            }
+
+            @Override
+            public void addQueryFieldKeys(Set<FieldKey> keys)
+            {
+                super.addQueryFieldKeys(keys);
+
+                keys.add(getBoundKey("species"));
+                keys.add(getBoundKey("trackName"));
+                keys.add(getBoundKey("releaseId", "jbrowseId"));
+                keys.add(getBoundKey("releaseId", "container"));
+                keys.add(getBoundKey("releaseId", "species"));
             }
         };
     }
