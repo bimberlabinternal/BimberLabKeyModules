e9feeddb95aa3d048e7f6b31ef1e8dbf646674c5
bbimber <bbimber@gmail.com>
2025-05-18 07:57:23 -0700

    Update download docs

--- mGAP/resources/web/mGAP/DownloadWindow.js
+++ mGAP/resources/web/mGAP/DownloadWindow.js
@@ -61,18 +61,18 @@ Ext4.define('mGAP.window.DownloadWindow', {
                 var toAdd = [{
                     html: 'Due to the large file size, the preferred option is to download using wget or curl on the command line, such as the exmaples below. Nonetheless, you also are able to paste the URLs into your browser and download through this way as well, although it will be slower and possibly not able to resume if your connection is disrupted.<br><br>' +
                             'Use these to download the VCF and index:<br>' +
-                            '<pre>wget https://mgapdownload.ohsu.edu/' + releaseVcf + '<br>' +
-                            'wget https://mgapdownload.ohsu.edu/' + releaseVcf + '.tbi</pre>' +
+                            '<pre>curl -C https://mgapdownload.ohsu.edu/' + releaseVcf + '<br>' +
+                            'curl -C https://mgapdownload.ohsu.edu/' + releaseVcf + '.tbi</pre>' +
                             (sitesOnlyVcf ?
                             'or a VCF without genotypes (considerably smaller):<br>' +
-                            '<pre>wget https://mgapdownload.ohsu.edu/' + sitesOnlyVcf + '<br>' +
-                            'wget https://mgapdownload.ohsu.edu/' + sitesOnlyVcf + '.tbi</pre>' : '') +
+                            '<pre>curl -C https://mgapdownload.ohsu.edu/' + sitesOnlyVcf + '<br>' +
+                            'curl -C https://mgapdownload.ohsu.edu/' + sitesOnlyVcf + '.tbi</pre>' : '') +
                             'and genome:<br>' +
-                            '<pre>wget https://mgapdownload.ohsu.edu/genomes/' + urlFasta + '<br>' +
-                            'wget https://mgapdownload.ohsu.edu/genomes/' + urlFasta + '.fai<br>' +
-                            'wget https://mgapdownload.ohsu.edu/genomes/' + urlFasta.replace(/fasta$/, 'dict') + '</pre>'
+                            '<pre>curl -C https://mgapdownload.ohsu.edu/genomes/' + urlFasta + '<br>' +
+                            'curl -C https://mgapdownload.ohsu.edu/genomes/' + urlFasta + '.fai<br>' +
+                            'curl -C https://mgapdownload.ohsu.edu/genomes/' + urlFasta.replace(/fasta$/, 'dict') + '</pre>'
                 },{
-                    html: '<br><b>mGAP is an NIH funded project.  If you use these data in a publication, we ask that you please include R24OD021324 in the acknowledgements.</b>',
+                    html: '<br><b>mGAP is an NIH funded project.  If you use these data in a publication, we ask that you please include R24 OD021324 in the acknowledgements.</b>',
                     border: false,
                     style: 'padding-bottom: 20px;'
                 }];
