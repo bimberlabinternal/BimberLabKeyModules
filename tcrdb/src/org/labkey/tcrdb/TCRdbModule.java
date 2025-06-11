df263c7a11c1d7489e164cd4d0b6dd3828584903
bbimber <bbimber@gmail.com>
2025-06-11 05:15:05 -0700

    Refactor several TCR tables

--- tcrdb/src/org/labkey/tcrdb/TCRdbModule.java
+++ tcrdb/src/org/labkey/tcrdb/TCRdbModule.java
@@ -46,7 +46,7 @@ public class TCRdbModule extends ExtendedSimpleModule
     @Override
     public Double getSchemaVersion()
     {
-        return 15.52;
+        return 15.53;
     }
 
     @Override
--- tcrdb/src/org/labkey/tcrdb/TCRdbProvider.java
+++ tcrdb/src/org/labkey/tcrdb/TCRdbProvider.java
@@ -69,6 +69,8 @@ public class TCRdbProvider extends AbstractSequenceDataProvider
         }
 
         items.add(new QueryImportNavItem(this, TCRdbSchema.NAME, TCRdbSchema.TABLE_CLONES, "TCR Clones", LaboratoryService.NavItemCategory.data, NAME, cache));
+        items.add(new QueryImportNavItem(this, TCRdbSchema.NAME, TCRdbSchema.TABLE_CLONE_RESPONSES, "TCR Clonotype Responses", LaboratoryService.NavItemCategory.data, NAME, cache));
+
         return Collections.unmodifiableList(items);
     }
 
