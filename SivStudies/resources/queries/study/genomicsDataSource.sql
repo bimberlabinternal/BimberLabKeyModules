b1fb0d40829b0b33c217df8765b1955d281a2e68
bbimber <bbimber@gmail.com>
2025-06-23 10:15:04 -0700

    Switch from librarytype to application in genomics query

--- SivStudies/resources/queries/study/genomicsDataSource.sql
+++ SivStudies/resources/queries/study/genomicsDataSource.sql
@@ -3,12 +3,12 @@ SELECT
 s1.subjectId as Id,
 s1.date,
 
-('Assay(s): ' || librarytype) as description
+('Assay(s): ' || application) as description
 
 FROM (SELECT
     s.subjectId,
     coalesce(s.sampleDate, now()) as date,
-    GROUP_CONCAT(DISTINCT s.librarytype, ', ') as librarytype
+    GROUP_CONCAT(DISTINCT s.application, ', ') as application
 
     FROM "/Internal/ColonyData".sequenceanalysis.sequence_readsets s
     GROUP BY s.subjectId, s.sampleDate
