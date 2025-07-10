20283e50139d3421e6230cb9196c8b576564b627
bbimber <bbimber@gmail.com>
2025-07-10 13:17:49 -0700

    Change date delimiter

--- SivStudies/resources/queries/study/demographicsChallengeAndArt.sql
+++ SivStudies/resources/queries/study/demographicsChallengeAndArt.sql
@@ -1,11 +1,11 @@
 SELECT
   t.Id,
   group_concat(DISTINCT CASE
-      WHEN t.category = 'SIV Infection' THEN (cast(month(t.date) as varchar) || '-' || cast(dayofmonth(t.date) as varchar) || '-' || cast(year(t.date) as varchar) || ' (' || t.treatment || ')')
+      WHEN t.category = 'SIV Infection' THEN (cast(month(t.date) as varchar) || '/' || cast(dayofmonth(t.date) as varchar) || '/' || cast(year(t.date) as varchar) || ' (' || t.treatment || ')')
       ELSE NULL
   END, char(10)) as allInfections,
   group_concat(DISTINCT CASE
-      WHEN t.category = 'ART' THEN (cast(month(t.date) as varchar) || '-' || cast(dayofmonth(t.date) as varchar) || '-' || cast(year(t.date) as varchar) || ' (' || t.treatment || ')')
+      WHEN t.category = 'ART' THEN (cast(month(t.date) as varchar) || '/' || cast(dayofmonth(t.date) as varchar) || '/' || cast(year(t.date) as varchar) || ' (' || t.treatment || ')')
       ELSE NULL
   END, char(10)) as allART,
 
