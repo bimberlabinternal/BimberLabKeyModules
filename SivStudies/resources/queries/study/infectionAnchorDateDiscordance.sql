c04c297f9e7ea45a9534c531802164433d720983
bbimber <bbimber@gmail.com>
2025-08-07 17:42:45 -0700

    Add study validation notification

--- SivStudies/resources/queries/study/infectionAnchorDateDiscordance.sql
+++ SivStudies/resources/queries/study/infectionAnchorDateDiscordance.sql
@@ -6,8 +6,11 @@ SELECT
 
 FROM study.treatments t
 FULL JOIN studies.subjectAnchorDates t2 ON (
+    t.category = 'SIV Infection' AND
     t2.subjectId = t.Id AND
     t2.eventLabel = 'SIV Infection'
 )
 
-WHERE t.category = 'SIV Infection' AND t2.date != t.date
+WHERE t2.date != t.date OR
+    t2.date IS NULL OR
+    t.date IS NULL
