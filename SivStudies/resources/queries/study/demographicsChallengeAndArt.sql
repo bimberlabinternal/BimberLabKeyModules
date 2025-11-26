67c66bf7db0d82c45e98f71fcc2b60b46af3ac09
bbimber <bbimber@gmail.com>
2025-11-26 15:53:07 -0800

    More query improvements

--- SivStudies/resources/queries/study/demographicsChallengeAndArt.sql
+++ SivStudies/resources/queries/study/demographicsChallengeAndArt.sql
@@ -11,7 +11,7 @@ FROM (
       min(floor(age(t.DataSets.Demographics.birth, CASE WHEN t.category = 'SIV Infection' THEN t.date ELSE NULL END))) AS ageAtInfection,
 
       group_concat(DISTINCT CASE
-          WHEN t.category = 'ART' THEN (t.treatment || ' (' || t.timePostSivChallenge.timePostInfection || ')')
+          WHEN t.category = 'ART' THEN (t.treatment || ' (' || COALESCE(t.timePostSivChallenge.timePostInfection, 'Unk DPI') || ')')
           ELSE NULL
       END, char(10)) as allART,
       min(CASE
--- SivStudies/resources/queries/study/demographicsInterventions.sql
+++ SivStudies/resources/queries/study/demographicsInterventions.sql
@@ -1,6 +1,6 @@
 SELECT
     t.Id,
-    group_concat(DISTINCT (t.treatment || ' (' || t.timePostSivChallenge.timePostInfection || ')'), char(10)) as allInterventions,
+    group_concat(DISTINCT (t.treatment || ' (' || COALESCE(t.timePostSivChallenge.timePostInfection, 'Unk DPI') || ')'), char(10)) as allInterventions,
     min(t.date) as firstInterventionDate,
     min(t.timePostSivChallenge.daysPostInfection) as firstInterventionDPI,
     min(t.timePostSivChallenge.weeksPostInfection) as firstInterventionWPI,
