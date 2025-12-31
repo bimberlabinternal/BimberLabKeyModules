04f14694b8f6650c1b68483a0c50ead518a378e7
bbimber <bbimber@gmail.com>
2025-12-31 10:22:57 -0800

    Add columns to cloneResponses

---
+++ tcrdb/resources/schemas/dbscripts/postgresql/tcrdb-15.60-15.61.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tcrdb.clone_responses ADD COLUMN cloneName varchar(1000);
+ALTER TABLE tcrdb.clone_responses ADD COLUMN cdr3WithSegments varchar(4000);
---
+++ tcrdb/resources/schemas/dbscripts/sqlserver/tcrdb-15.60-15.61.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tcrdb.clone_responses ADD cloneName varchar(1000);
+ALTER TABLE tcrdb.clone_responses ADD cdr3WithSegments varchar(4000);
