60b329160981029b9cd33984c0b8f8a30d77c01e
bbimber <bbimber@gmail.com>
2026-01-20 16:17:38 -0800

    Add new field

--- tcrdb/src/org/labkey/tcrdb/TCRdbModule.java
+++ tcrdb/src/org/labkey/tcrdb/TCRdbModule.java
@@ -46,7 +46,7 @@ public class TCRdbModule extends ExtendedSimpleModule
     @Override
     public Double getSchemaVersion()
     {
-        return 15.63;
+        return 15.64;
     }
 
     @Override
