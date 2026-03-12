2da691dc4a4f31569c24c5cc597497d89a46c856
bbimber <bbimber@gmail.com>
2026-03-12 10:05:55 -0700

    Update to MCC test

--- mcc/test/src/org/labkey/test/tests/mcc/MccTest.java
+++ mcc/test/src/org/labkey/test/tests/mcc/MccTest.java
@@ -182,6 +182,7 @@ public class MccTest extends BaseWebDriverTest
         waitAndClick(Ext4Helper.Locators.ext4Button("OK"));
         sleep(100);
 
+        Ext4FieldRef.getForLabel(this, "Request ID").setValue(12345);
         Ext4ComboRef.getForLabel(this, "Target Folder").setComboByDisplayValue("Other");
         _ext4Helper.queryOne("#usePreviousId-Animal2", Ext4FieldRef.class).setChecked(true);
         waitAndClick(Ext4Helper.Locators.ext4Button("Submit"));
@@ -239,6 +240,7 @@ public class MccTest extends BaseWebDriverTest
         waitAndClick(Ext4Helper.Locators.ext4Button("OK"));
         sleep(100);
 
+        Ext4FieldRef.getForLabel(this, "Request ID").setValue(12345);
         Ext4ComboRef.getForLabel(this, "Target Folder").setComboByDisplayValue("Other");
         _ext4Helper.queryOne("#newId-12345", Ext4FieldRef.class).setValue("TheNewId");
         waitAndClick(Ext4Helper.Locators.ext4Button("Submit"));
